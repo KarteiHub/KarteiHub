3a37f504cd3599c1e00cb8178664ed89ebedc951
Silthus <md.doering@yahoo.de>
2012-01-14 17:44:11 +0100

    added js files

---
+++ js/KarteiHub.js
@@ -0,0 +1,66 @@
+KarteiHub = function() {
+	this.cards;
+	this.boxes;
+}
+
+KarteiHub.showAwnser = function(boolean showAwnser) {
+	if (showSolution) {
+		KarteiHubUI.showAwnser(getActiveCard());
+	} else {
+		KarteiHubUI.showQuestion(getActiveCard());
+	}
+}
+
+KarteiHub.awnser = function(boolean correct) {
+	if (correct) {
+		$.post("../action.php", { 	action: "right",
+									id: KarteiHub.getActiveCard().getId() },
+				function (html) {
+					KarteiHubUI.showNextCard();
+				});
+	} else {
+		$.post("../action.php", { 	action: "wrong",
+									id: KarteiHub.getActiveCard().getId() },
+				function (html) {
+					KarteiHubUI.showNextCard();
+				});
+	}
+}
+
+KarteiHub.loadCards = function() {
+	$.post("../action.php", { action: "getCards" },
+			function (html) {
+				// check for errors and diffrent response messages
+				this.cards = new Array(html);
+			});
+}
+
+KarteiHub.loadBoxes = function() {
+	$.post("../action.php", { action: "getBoxes" },
+			function (html) {
+				// need to check for correct formatting later
+				boxes = new Array(html);
+				this.boxes = new Array();
+				for (int i = 0; i < boxes.size(); i++) {
+					this.boxes[i] = new Box(boxes[i]);
+				}
+			});
+}
+
+KarteiHub.storeCard = function() {
+	KarteiHubUI.storeCard(KarteiHub.getActiveCard().getId());
+}
+
+KarteiHub.getActiveCard = function() {
+	return this.cards[0];
+}
+
+KarteiHub.orderStack = function() {
+	for (int i = 0; i < cards.size(); i++) {
+		if (i == cards.size-1) {
+			cards[i] = null;
+			break;
+		}
+		cards[i] = cards[i+1];
+	}
+}
